c4a20eab07b0d92b610036d3e19c07e27c814a4e
Souarv Sharma <souravgopal25@gmail.com>
2020-09-28 12:42:15 +0530

    bugs fixed

--- lib/police_account/police_signup_page1.dart
+++ lib/police_account/police_signup_page1.dart
@@ -1,152 +1,128 @@
+import 'package:ap_ps_side/police_account/police_signup_page2.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'file:///C:/Users/shres/AndroidStudioProjects/ps_fir_user/lib/Police_Account/police_account_page2.dart';
 
 class Police_Account_Page1 extends StatefulWidget {
-  Police_Account_Page1 ({Key key, this.title}) : super (key : key);
+  Police_Account_Page1({Key key, this.title}) : super(key: key);
 
   final String title;
 
   _Police_Account_Page1 createState() => _Police_Account_Page1();
-
 }
 
 class _Police_Account_Page1 extends State<Police_Account_Page1> {
-
   String name;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(),
+      appBar: AppBar(),
       body: SingleChildScrollView(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-
-            children: <Widget>[
-
-              SizedBox(
-                height: 20.0,
-              ),
-
-              CircleAvatar(
-                backgroundColor: Colors.blue,
-                radius: 20.0,
-              ),
-
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
-                child: Divider(),
-              ),
-
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.account_circle),
-                    labelText: 'Name',
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.blue
-                      ),
-                    ),
-                    border: OutlineInputBorder(),
-                  ),
-
-                  onChanged: (value) {
-                    setState(() {
-                      name = value;
-                    });
-                  },
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(
+                  height: 20.0,
                 ),
-
-              ),
-
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.account_circle),
-                    labelText: 'Email',
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.blue
-                      ),
-                    ),
-                    border: OutlineInputBorder(),
-                  ),
-
-                  onChanged: (value) {
-                    setState(() {
-                      name = value;
-                    });
-                  },
+                CircleAvatar(
+                  backgroundColor: Colors.blue,
+                  radius: 50.0,
                 ),
-
-              ),
-
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.account_circle),
-                    labelText: 'Phone No.',
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.blue
-                      ),
-                    ),
-                    border: OutlineInputBorder(),
-                  ),
-
-                  onChanged: (value) {
-                    setState(() {
-                      name = value;
-                    });
-                  },
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+                  child: Divider(),
                 ),
-
-              ),
-
-              Padding(
-                padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
-                child: TextField(
-                  decoration: InputDecoration(
-                    prefixIcon: Icon(Icons.account_circle),
-                    labelText: 'Address',
-                    enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.blue
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.account_circle),
+                      labelText: 'Name',
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
                       ),
+                      border: OutlineInputBorder(),
                     ),
-                    border: OutlineInputBorder(),
+                    onChanged: (value) {
+                      setState(() {
+                        name = value;
+                      });
+                    },
                   ),
-
-                  onChanged: (value) {
-                    setState(() {
-                      name = value;
-                    });
-                  },
                 ),
-              ),
-            ],
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.account_circle),
+                      labelText: 'Email',
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        name = value;
+                      });
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(
+                        Icons.account_circle,
+                      ),
+                      labelText: 'Phone No.',
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        name = value;
+                      });
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 8.0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.account_circle),
+                      labelText: 'Address',
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue),
+                      ),
+                      border: OutlineInputBorder(),
+                    ),
+                    onChanged: (value) {
+                      setState(() {
+                        name = value;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
-
       floatingActionButton: FloatingActionButton(
         elevation: 5.0,
         child: new Icon(Icons.chevron_right),
         backgroundColor: Colors.blue,
         onPressed: () {
           Navigator.of(context).push(MaterialPageRoute(
-              builder: (BuildContext context) => (Police_Account_Page2())
-          ));
+              builder: (BuildContext context) => (Police_Account_Page2())));
         },
       ),
-
     );
   }
-
 }
--- lib/police_account/police_signup_page2.dart
+++ lib/police_account/police_signup_page2.dart
@@ -3,16 +3,14 @@ import 'package:flutter/cupertino.dart';
 import 'package:file_picker/file_picker.dart';
 
 class Police_Account_Page2 extends StatefulWidget {
-  Police_Account_Page2 ({Key key, this.title}) : super (key : key);
+  Police_Account_Page2({Key key, this.title}) : super(key: key);
 
   final String title;
 
   _Police_Account_Page2 createState() => _Police_Account_Page2();
-
 }
 
 class _Police_Account_Page2 extends State<Police_Account_Page2> {
-
   String designation;
   String state;
   String district;
@@ -22,28 +20,23 @@ class _Police_Account_Page2 extends State<Police_Account_Page2> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar:AppBar(),
+      appBar: AppBar(),
       body: SingleChildScrollView(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
-
             children: <Widget>[
-
               SizedBox(
                 height: 20.0,
               ),
-
               CircleAvatar(
                 backgroundColor: Colors.blue,
                 radius: 20.0,
               ),
-
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
                 child: Divider(),
               ),
-
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
                 child: TextField(
@@ -51,22 +44,17 @@ class _Police_Account_Page2 extends State<Police_Account_Page2> {
                     prefixIcon: Icon(Icons.account_circle),
                     labelText: 'Designation',
                     enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.blue
-                      ),
+                      borderSide: BorderSide(color: Colors.blue),
                     ),
                     border: OutlineInputBorder(),
                   ),
-
                   onChanged: (value) {
                     setState(() {
                       designation = value;
                     });
                   },
                 ),
-
               ),
-
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
                 child: TextField(
@@ -74,22 +62,17 @@ class _Police_Account_Page2 extends State<Police_Account_Page2> {
                     prefixIcon: Icon(Icons.account_circle),
                     labelText: 'State',
                     enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.blue
-                      ),
+                      borderSide: BorderSide(color: Colors.blue),
                     ),
                     border: OutlineInputBorder(),
                   ),
-
                   onChanged: (value) {
                     setState(() {
                       state = value;
                     });
                   },
                 ),
-
               ),
-
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
                 child: TextField(
@@ -97,22 +80,17 @@ class _Police_Account_Page2 extends State<Police_Account_Page2> {
                     prefixIcon: Icon(Icons.account_circle),
                     labelText: 'District',
                     enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.blue
-                      ),
+                      borderSide: BorderSide(color: Colors.blue),
                     ),
                     border: OutlineInputBorder(),
                   ),
-
                   onChanged: (value) {
                     setState(() {
                       district = value;
                     });
                   },
                 ),
-
               ),
-
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
                 child: TextField(
@@ -120,13 +98,10 @@ class _Police_Account_Page2 extends State<Police_Account_Page2> {
                     prefixIcon: Icon(Icons.account_circle),
                     labelText: 'Police Station',
                     enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.blue
-                      ),
+                      borderSide: BorderSide(color: Colors.blue),
                     ),
                     border: OutlineInputBorder(),
                   ),
-
                   onChanged: (value) {
                     setState(() {
                       police_station = value;
@@ -134,8 +109,6 @@ class _Police_Account_Page2 extends State<Police_Account_Page2> {
                   },
                 ),
               ),
-
-
               Padding(
                 padding: EdgeInsets.symmetric(vertical: 1.0, horizontal: 1.0),
                 child: TextField(
@@ -143,13 +116,10 @@ class _Police_Account_Page2 extends State<Police_Account_Page2> {
                     prefixIcon: Icon(Icons.account_circle),
                     labelText: 'Identity Card',
                     enabledBorder: OutlineInputBorder(
-                      borderSide: BorderSide(
-                          color: Colors.blue
-                      ),
+                      borderSide: BorderSide(color: Colors.blue),
                     ),
                     border: OutlineInputBorder(),
                   ),
-
                   onChanged: (value) {
                     setState(() {
                       id = value;
@@ -157,12 +127,12 @@ class _Police_Account_Page2 extends State<Police_Account_Page2> {
                   },
                 ),
               ),
-
               RaisedButton(
-                onPressed: () async{
-                  FilePickerResult result = await FilePicker.platform.pickFiles();
+                onPressed: () async {
+                  FilePickerResult result =
+                      await FilePicker.platform.pickFiles();
 
-                  if(result != null) {
+                  if (result != null) {
                     PlatformFile file = result.files.first;
 
                     print(file.name);
@@ -173,24 +143,16 @@ class _Police_Account_Page2 extends State<Police_Account_Page2> {
                   }
                 },
               )
-
             ],
           ),
         ),
       ),
-
       floatingActionButton: FloatingActionButton(
         elevation: 5.0,
         child: new Icon(Icons.chevron_right),
         backgroundColor: Colors.blue,
-        onPressed: () {
-          Navigator.of(context).push(MaterialPageRoute(
-              builder: (BuildContext context) => ()
-          ));
-        },
+        onPressed: () {},
       ),
-
     );
   }
-
 }
