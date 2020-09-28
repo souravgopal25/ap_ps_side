b8d5b950fa78672976917c93ceec83308b6cd71c
shrestha24 <67696266+shrestha24@users.noreply.github.com>
2020-09-28 19:59:46 +0530

    option page for both police and admin.

--- lib/admin/navigation_pane_admin.dart
+++ lib/admin/navigation_pane_admin.dart
@@ -55,7 +55,7 @@ class Menu extends StatelessWidget {
               style: TextStyle(fontSize: 25.0),
             ),
           ),
-          
+
           ListTile(
             leading: Icon(
               Icons.perm_device_information,
---
+++ lib/page/option_police/admin.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+
+
+class LoginPage extends StatefulWidget {
+  LoginPage({Key key}) : super(key: key);
+
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          child: Row(
+            children: <Widget>[
+              Image.asset(
+                "assets/ashoka.jpeg",
+                height: 100,
+                fit: BoxFit.contain,
+              ),
+              SizedBox(
+                width: 20,
+              ),
+              Column(
+                children: [
+                  Text(
+                    "GOVERMENT OF ASSAM",
+                    style: TextStyle(color: Colors.black, fontSize: 25),
+                  ),
+                  Text(
+                    "ASSAM POLICE",
+                    style: TextStyle(
+                        color: Colors.blue[400],
+                        fontWeight: FontWeight.bold,
+                        fontSize: 25),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+        toolbarHeight: 120,
+        backgroundColor: Colors.white,
+        iconTheme: IconThemeData(color: Colors.black),
+      ),
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Image.network(
+              "https://images.unsplash.com/photo-1589441931743-900205c41d7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&w=1000&q=80",
+              fit: BoxFit.cover,
+            ),
+          ),
+          Center(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Card(
+                  elevation: 15,
+                  child: Column(
+                    children: <Widget>[
+                      Container(
+                        child: Text(
+                          "Login",
+                          style: TextStyle(fontSize: 100, color: Colors.blue),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
